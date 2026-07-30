0589cb8de553818b68ff0699749750620a47b353
sasasz3 <sara.szoboszlai.2023@uni.strath.ac.uk>
2026-07-30 11:19:53 +0100

    stop tracking config

--- config.R
+++
@@ -1,2 +0,0 @@
-PROJECT_FOLDER  <- getwd()
-DATA_FOLDER <- "C:/Users/nxb23156/OneDrive - University of Strathclyde/Data"
--- config.py
+++
@@ -1,9 +0,0 @@
-from pathlib import Path
-
-PROJECT_FOLDER = Path(
-    r"C:\Users\nxb23156\Documents\OneDrive\Ch1"
-)
-
-DATA_FOLDER = Path(
-    r"C:\Users\nxb23156\OneDrive - University of Strathclyde\Data"
-)
