f98055da898ad27fc09cfc9e9c7cddc2a6891e4f
sasasz3 <sara.szoboszlai.2023@uni.strath.ac.uk>
2026-08-04 17:30:43 +0100

    compustat employment data

--- employment.R
+++ employment.R
@@ -8,3 +8,32 @@ employment <- setDT(
     sheet = "Results"
   )
 )
+
+clean_and_rename(employment)
+fix_gvkey(employment)
+
+setDT(employment)
+
+setorder(employment, gvkey, fyear)
+
+employment[, emp_prev := shift(emp), by = gvkey]
+
+employment[, emp_change := (emp - emp_prev) / emp_prev]
+
+employment[, change_10_pct := emp_change <= -0.10]
+
+employment[, change_5_pct := emp_change <= -0.05]
+
+
+num_decrease_10 <- employment[change_10_pct == TRUE, .N]
+num_decrease_5 <- employment[change_5_pct == TRUE, .N]
+
+num_decrease_10
+num_decrease_5
+
+
+firm_10_decrease <- employment[change_10_pct == TRUE, uniqueN(gvkey)]
+firm_5_decrease <- employment[change_5_pct == TRUE, uniqueN(gvkey)]
+
+firm_10_decrease
+firm_5_decrease
